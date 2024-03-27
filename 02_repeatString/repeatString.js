38a7b0bb314665680d2da1f3473afa5aa3a424bd
nqlp <nqlpaul@gmail.com>
2024-03-27 09:39:25 -0400

    Fix repeatString function to handle negative numbers and repeat the string correctly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    if (num >= 0) {
+        for (let i = 0; i <= num; i++) {
+            return string.repeat(num);
+        }
+    }
 };
 
 // Do not edit below this line
