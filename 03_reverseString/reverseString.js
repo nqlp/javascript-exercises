b9b7df1dd18170e1cf88860bbc6e07cf7ee8af14
nqlp <nqlpaul@gmail.com>
2024-03-27 11:57:25 -0400

    Implement reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
