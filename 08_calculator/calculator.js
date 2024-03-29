a2c5e3532b2c4cf71f4d08b1aa1724ffb5040920
nqlp <nqlpaul@gmail.com>
2024-03-29 15:05:48 -0400

    Implement math functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  else if (array.length === 1) {
+    return array[0];
+  }
+
+  // computes the sum of an array of many numbers
+  else if (array.length > 2) {
+    return array.reduce((acc, curr) => acc + curr);
+  }
+
+  else {
+    for (let i = 0; i < array.length; i++) {
+      return array[i] + array[i + 1];
+    }
+  }
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  if (array.length > 2) {
+    return array.reduce((acc, curr) => acc * curr);
+  }
 
+  for (let i = 0; i < array.length; i++) {
+    return array[i] * array[i + 1];
+  }
 };
 
-const power = function() {
-	
+const power = function (firstNumber, secondNumber) {
+  return Math.pow(firstNumber, secondNumber);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  }
+  else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
