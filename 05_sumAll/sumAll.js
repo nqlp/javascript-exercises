e0ed27fec4ad2dac62d8df1cf7acb90ea853cc64
nqlp <nqlpaul@gmail.com>
2024-03-29 12:42:08 -0400

    implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+
+    else if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+        return "ERROR";
+    }
+
+    else if (firstNum > secondNum) {
+        let temp = firstNum;
+        firstNum = secondNum;
+        secondNum = temp;
+        let finalSum = 0;
+        for (let i = firstNum; i <= secondNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
+
+    else {
+        let finalSum = 0;
+        for (let i = firstNum; i <= secondNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 
 };
 
